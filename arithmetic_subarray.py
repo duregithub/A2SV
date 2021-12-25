209df7b275c5fc9c0cf70cc36345125d34b25293
duregithub <duressajemal3@gmail.com>
2021-12-25 11:29:39 +0300

    leetcode arithmetic subarray

---
+++ arithmetic_subarray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkArithmeticSubarrays(self, nums, l, r):
+        
+        output = []
+        
+        for i in range(len(l)):
+              output.append(self.checker(nums[ l[i]:r[i] + 1 ]))
+        return output
+        
+    def checker(self, num):
+        print(num)
+        
+        num.sort()
+        for i in range(len(num)):
+            if len(num) == 2:
+                return True
+            if i + 1 == len(num) or i + 2 == len(num):
+                break
+            if num[i] - num[i+1] != num[i+1] - num[i+2]:
+                return False
+        return True
+        
